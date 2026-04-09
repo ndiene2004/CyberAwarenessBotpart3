3b11d6b258e581731258073ac51a18d63fe73108
ndiene2004 <mukondelelindiene68@gmail.com>
2026-04-09 12:01:50 +0200

    Added 3 more comments to methods

---
+++ Downloads/CybersecurityChatbot_fixed/Program.cs
@@ -0,0 +1,5 @@
+// This method displays the ASCII art logo
+static void DisplayASCIIArt()// This method plays a WAV audio greeting when the program starts
+static void PlayVoiceGreeting()// This method runs the main chat loop and keeps the conversation going
+static void ChatLoop(string userName)// This method checks what the user typed and returns the correct response
+static string GetResponse(string userInput)
