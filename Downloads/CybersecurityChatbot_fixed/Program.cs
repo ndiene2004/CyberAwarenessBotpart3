64a0d2ff1b573551a6eb60cadc7b23e220dad441
ndiene2004 <mukondelelindiene68@gmail.com>
2026-04-09 12:15:12 +0200

    Final submission - completed cybersecurity chatbot

--- Downloads/CybersecurityChatbot_fixed/Program.cs
+++ Downloads/CybersecurityChatbot_fixed/Program.cs
@@ -2,4 +2,4 @@
 static void DisplayASCIIArt()// This method plays a WAV audio greeting when the program starts
 static void PlayVoiceGreeting()// This method runs the main chat loop and keeps the conversation going
 static void ChatLoop(string userName)// This method checks what the user typed and returns the correct response
-static string GetResponse(string userInput)
+static string GetResponse(string userInput)Final submission - Ndiene Mukondeleli
